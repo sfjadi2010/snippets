3a4dfebc4cd0a9f1a0dcf96559c65f74921c42cf
Salman Farsi <sfjadi@gmail.com>
2024-04-13 23:17:17 -0400

    Refactor page component to display snippets from the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import {db} from "@/db";
 
-export default function Home() {
-  async function SnippetsList() {
+export default async function Snippets() {
+  const snippets = await db.snippet.findMany();
 
-  }
+  const renderdSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h3 className="text-xl">{snippet.title}</h3>
+        <pre>{snippet.code}</pre>
+        <br />
+      </div>
+    );
+  });
   return (
-    <div>Snippets</div>
+    <div>
+      <h1>Snippets</h1>
+      <div>
+        <a className="text-blue-300 border rounded p-1" href="/snippets/new">Create a Snippet</a>
+      </div>
+      <br />
+      <div>{renderdSnippets}</div>
+    </div>
+    
   );
 }
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+import {db} from "@/db";
+
+interface SnippetShowPageProps {
+    params: {
+        id: string;
+    }
+}
+export default async function SnippetShowPage(props: SnippetShowPageProps)
+{
+    const snippet = await db.snippet.findFirst({
+        where: {
+            id: parseInt(props.params.id),
+        },
+    });
+
+    if (!snippet) {
+        return notFound();
+    }
+
+    return (
+        <div>
+            <h1>Snippet</h1>
+            <h3>{snippet.title}</h3>
+            <pre>{snippet.code}</pre>
+        </div>
+    );
+}
