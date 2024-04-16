8a6e84fd425c3689414f18e777a6cd8d851054aa
Salman Farsi <sfjadi@gmail.com>
2024-04-16 13:34:44 -0400

    Refactor createSnippet function to validate user inputs and create a new record in the database

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -25,3 +25,36 @@ export async function deleteSnippet(id: number) {
 
     redirect(`/`);
 }
+
+export async function createSnippet(formState: {message: String}, formData: FormData) {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title');
+    const code = formData.get('code');
+
+    // Check if title and code are present
+    if (!title || !code) {
+        return { message: 'Title and code are required' };
+    }
+
+    // Title validation must be string and at least 3 characters
+    if (typeof title !== 'string' || title.length < 3) {
+        return { message: 'Title must be longer' };
+    }
+
+    // Code validation must be string and at least 10 characters
+    if (typeof code !== 'string' || title.length < 10)
+    {
+        return { message: 'Code must be long'};
+    }
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code,
+        },
+    });
+    
+    // Redirect the user back to the root route
+    redirect('/');
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,28 +1,12 @@
-import {db} from "@/db"
-import { redirect } from "next/navigation";
+'use client';
+
+import {useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-    async function createSnippet(formData: FormData) {
-        // This needs to be a server actions!
-        'use server';
-
-        // Check the user's inputs and make sure they're valid
-        const title = formData.get("title") as string;
-        const code = formData.get("code") as string;
-
-        // Create a new record in the database
-        const snippet = await db.snippet.create({
-            data: {
-                title,
-                code,
-            },
-        });
-        
-        // Redirect the user back to the root route
-        redirect('/');
-    }
+    const [formState, action] = useFormState(actions.createSnippet, {message: ''});
     return (
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className="font-bold m-3">Create a Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
@@ -34,7 +18,13 @@ export default function SnippetCreatePage() {
                     <label className="w-12" htmlFor="code">Code</label>
                     <textarea className="border rounded p-2 w-full" name="code" id="code" />
                 </div>
-
+                {
+                    formState.message ?
+                        <div className='my-2 p-2 bg-red-200 border rounded border-red-400'>
+                            {formState.message}
+                        </div>  
+                    : null
+                }
                 <button type="submit" className="rounded p-2 bg-blue-200">
                     Create Snippet
                 </button>
